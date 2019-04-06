cf95ae2985b7c4d05ae9478701a2b5d2c58c352c
shun-sasano <jumpjudo3303@gmail.com>
2019-04-06 15:40:51 +0900

    Initial Commit

---
+++ LoverProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoverProfile
+//
+//  Created by 笹野駿 on 2019/04/06.
+//  Copyright © 2019 笹野駿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
